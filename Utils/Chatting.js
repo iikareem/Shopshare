5016d378939d5f2e9790daca551bccd2a079d91f
iikareem <77942683+iikareem@users.noreply.github.com>
2023-05-16 15:03:24 +0300

    Update Chatting.js

--- Utils/Chatting.js
+++ Utils/Chatting.js
@@ -1,26 +1,26 @@
-const socket = require('socket.io');
+// const socket = require('socket.io');
 
-function setupSocket(server) {
+// function setupSocket(server) {
 
-    // lazem a3rfo el server ely hysht8al 3lih
-    const io = socket(server);
+//     // lazem a3rfo el server ely hysht8al 3lih
+//     const io = socket(server);
 
-// kol request ykon leh his own socket
-    io.on('connection',(socket)=>{
+// // kol request ykon leh his own socket
+//     io.on('connection',(socket)=>{
 
         
-        socket.on("chat",(data)=>{
-            io.sockets.emit("chat",data);
-        });
+//         socket.on("chat",(data)=>{
+//             io.sockets.emit("chat",data);
+//         });
 
-        socket.on("typing",(data)=>{
-            socket.broadcast.emit("typing",data);
-        })
-    });
+//         socket.on("typing",(data)=>{
+//             socket.broadcast.emit("typing",data);
+//         })
+//     });
 
-    return io;
-}
+//     return io;
+// }
 
 
 
-module.exports = setupSocket;
+// module.exports = setupSocket;
